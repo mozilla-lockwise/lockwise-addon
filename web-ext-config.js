8842a3027083441ce242d83f2fca47e67d080b5d
Matthew A. Miller <linuxwolf@outer-planes.net>
2018-10-01 13:35:55 -0600

    web-ext run + web-ext config

---
+++ web-ext-config.js
@@ -0,0 +1,15 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+module.exports = {
+  run: {
+    pref: [
+      "toolkit.telemetry.enabled=true",
+      "toolkit.telemetry.server=https://127.0.0.1/telemetry-dummy/",
+      "xpinstall.signatures.required=false"
+    ],
+  },
+};
