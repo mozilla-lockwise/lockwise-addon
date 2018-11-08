4a964b3dc363175aa5a139e2e673e5a8b53a0d22
Matthew A. Miller <linuxwolf@outer-planes.net>
2018-11-08 11:37:05 +0700

    port of bootstrapping datastore

---
+++ src/background/datastore.js
@@ -0,0 +1,193 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import UUID from "uuid";
+
+function convertInfo2Item(info) {
+  let title;
+  try {
+    title = (new URL(info.hostname)).host;
+  } catch (ex) {
+    title = info.hostname;
+  }
+  title = title.replace(/^http(s)?:\/\//, "").
+                replace(/^www\./, "");
+
+  const id = info.guid;
+  const origins = [ info.hostname, info.formSubmitURL ].
+      filter((u) => !!u);
+
+  let item = {
+    id,
+    title,
+    origins,
+    realm: info.httpRealm,
+    tags: [],
+    entry: {
+      kind: "login",
+      username: info.username,
+      password: info.password,
+      usernameField: info.usernameField,
+      passwordField: info.passwordField,
+      notes: "",
+    },
+  };
+  return item;
+}
+function convertItem2Info(item) {
+  // dropped on the floor (for now ...)
+  // * title
+  // * tags
+  // * entry.notes
+  // * history
+
+  // item.id ==> info.guid
+  // item.title ==> undefined
+  // item.tags ==> undefined
+  // item.origins[0] ==> info.hostname
+  // item.origins[1] || null ==> info.formSubmitURL
+  // item.entry.kind ==> undefined
+  // item.entry.username ==> info.username
+  // item.entry.password ==> info.password
+  // item.entry.usernameField || "" ==> info.usernameField
+  // item.entry.passwordField || "" ==> info.passwordField
+  // item.entry.notes ==> info.undefined
+  const guid = item.id;
+  const hostname = item.origins[0];
+  const formSubmitURL = item.origins[1] || null;
+  const httpRealm = item.realm || null;
+  const username = item.entry.username;
+  const password = item.entry.password;
+  const usernameField = item.entry.usernameField || "";
+  const passwordField = item.entry.passwordField || "";
+
+  let info = {
+    guid,
+    hostname,
+    formSubmitURL,
+    httpRealm,
+    username,
+    password,
+    usernameField,
+    passwordField,
+  };
+
+  return info;
+}
+
+async function recordMetric(method, itemid, fields) {
+  let extra = {
+    itemid,
+  };
+  if (fields) {
+    extra = {
+      ...extra,
+      fields,
+    };
+  }
+  browser.telemetry.recordEvent(method, "datastore", extra);
+}
+
+class DataStore {
+  constructor() {
+    this._all = {};
+  }
+
+  async _allLogins() {
+    return Object.values(this._all);
+  }
+
+  async list() {
+    const logins = await this._allLogins();
+    const items = logins.map(convertInfo2Item);
+
+    return items;
+  }
+  async get(id) {
+    let one = this._all[id];
+    if (one) {
+      one = convertInfo2Item(one);
+    }
+
+    return one || null;
+  }
+  async add(item) {
+    if (!item) {
+      throw new TypeError("invalid item");
+    }
+
+    let info = convertItem2Info(item);
+    if (!info.hostname) {
+      throw new TypeError("missing hostname");
+    }
+    if (!info.password) {
+      throw new TypeError("missing password");
+    }
+    if (!info.username) {
+      info.username = "";
+    }
+
+    if (!info.formSubmitURL && !info.httpRealm) {
+      // assume formSubmitURL === hostname
+      info.formSubmitURL = info.hostname;
+    }
+    // TODO: call logins API
+    info = {
+      ...info,
+      guid: UUID(),
+      timeCreated: Date.now(),
+      timePasswordChanged: Date.now(),
+    };
+    this._all[info.guid] = info;
+
+    added = convertInfo2Item(info);
+    recordMetric("added", added.id);
+
+    return added;
+  }
+  async update(item) {
+    const id = item.id;
+    if (!id) {
+      throw new TypeError("id missing");
+    }
+
+    const info = convertItem2Info(item);
+    const orig = await this.get(id);
+    if (!orig) {
+      throw new Error(`no existing item for ${id}`);
+    }
+
+    // TODO: call API
+    let updated = {
+      ...info,
+      timePasswordChanged: Date.now(),
+    };
+
+    updated = convertInfo2Item(updated);
+    recordMetric("updated", item.id);
+
+    return updated;
+  }
+  async remove(id) {
+    const item = await this.get(id);
+    if (item) {
+      const login = convertItem2Info(item);
+
+      // TODO: call API
+      delete this._all[item.id];
+
+      recordMetric("deleted", item.id);
+    }
+    return item || null;
+  }
+
+}
+
+let bootstrap;
+export default async function openDataStore() {
+  if (!bootstrap) {
+    bootstrap = new DataStore();
+  }
+  return bootstrap;
+}
