c867750793825b26add92beb2ca6491902b93ea8
Jared Hirsch <ohai@6a68.net>
2019-01-10 14:18:47 -0800

    Fill in happy path tests

--- test/integration/logins-api-test.js
+++ test/integration/logins-api-test.js
@@ -18,6 +18,7 @@ const mockLogin = {
   timePasswordChanged: 1546291981955,
   timesUsed: 1,
   hostname: "https://example.com",
+  httpRealm: null,
   formSubmitURL: "https://example.com",
   usernameField: "username",
   passwordField: "password",
@@ -28,12 +29,61 @@ const mockLogin = {
 describe("logins API", () => {
   let webext;
   let driver;
+  let webdriver;
+  let By;
+  let until;
 
+  const addLogin = async () => {
+    await webext.inChrome();
+    await driver.executeScript(`
+      // XXX For some reason, template substitution doesn't work with selenium
+      const mockLogin = {
+        guid: "{33535344-9cdb-8c4a-ae10-5849d0a2f04a}",
+        timeCreated: 1546291981955,
+        timeLastUsed: 1546291981955,
+        timePasswordChanged: 1546291981955,
+        timesUsed: 1,
+        hostname: "https://example.com",
+        httpRealm: null,
+        formSubmitURL: "https://example.com",
+        usernameField: "username",
+        passwordField: "password",
+        username: "creativeusername",
+        password: "p455w0rd"
+      };
+      const login = LoginHelper.vanillaObjectToLogin(mockLogin);
+      Services.logins.addLogin(login);
+    `);
+  };
+
+  const getLogins = async () => {
+    await webext.inChrome();
+    const logins = await driver.executeScript(`
+      return Services.logins.getAllLogins().map(LoginHelper.loginToVanillaObject);
+    `);
+    return logins;
+  };
+
+  const loadTestPage = async () => {
+    await webext.inContent();
+    await driver.get(webext.url('/test/integration/test-pages/logins-api.html'));
+  };
+
+  const clickButton = async (id) => {
+    await webext.inContent();
+    const btn = await driver.wait(until.elementLocated(
+      By.id(id)
+    ), 1000);
+    await btn.click();
+  };
 
   before(async () => {
     webext = await getWebExtension();
     await webext.start();
     driver = await webext.driver;
+    webdriver = webext.webdriver;
+    By = webdriver.By;
+    until = webdriver.until;
     await webext.inChrome();
     await driver.executeScript(`
       ChromeUtils.defineModuleGetter(this, "LoginHelper",
@@ -50,48 +100,79 @@ describe("logins API", () => {
     `);
   });
 
-  afterEach(async () => {
-  });
-
   after(async () => {
     await driver.quit();
   });
 
   it("browser.experiments.logins.add should add a new login", async () => {
-    const { webdriver } = webext;
-    const { By, until } = webdriver;
-
-    // 1. verify no logins.
     await webext.inChrome();
-    const initialLogins = await driver.executeScript(`
-      return Services.logins.getAllLogins().map(LoginHelper.loginToVanillaObject);
-    `);
+    const initialLogins = await getLogins();
     expect(initialLogins).to.be.an('array').that.is.empty;
 
-    // 2. open test page & add login via API.
-    await webext.inContent();
-    await driver.get(webext.url('/test/integration/test-pages/logins-api.html'));
-    const btn = await driver.wait(until.elementLocated(
-      By.id("add")
-    ), 1000);
-    await btn.click();
-    // verify there's something in the output
-    const results = await driver.wait(until.elementLocated(
-      By.id("add-results")
-    ), 1000);
-    // temporarily insert a super long wait, so we can examine the running test
-    // TODO: why is browser.experiments.logins undefined when the test loads the URL,
-    // but available when I load the page from browser.tabs.create() in the about:debugger debugger?
-    await driver.wait(until.elementTextContains(results, 'guid'),30000);
-    const output = results.getText();
-    expect(output).to.equal('foo');
+    await loadTestPage();
+    await clickButton("add");
 
-    // 3. verify login.
     await webext.inChrome();
-    const finalLogin = await driver.executeScript(`
-      const login = Services.logins.getAllLogins()[0];
-      return LoginHelper.loginToVanillaObject(login);
-    `);
-    expect(finalLogin).to.deep.equal(mockLogin);
+    const finalLogins = await getLogins();
+    expect(finalLogins[0]).to.deep.equal(mockLogin);
+  });
+
+  it("browser.experiments.logins.remove should remove the login", async () => {
+    await addLogin();
+
+    await loadTestPage();
+    await clickButton("remove");
+
+    await webext.inChrome();
+    const results = await getLogins();
+    expect(results).to.be.an('array').that.is.empty;
+  });
+
+  it("browser.experiments.logins.get should return the login", async () => {
+    await addLogin();
+
+    await loadTestPage();
+    await clickButton("get");
+
+    const results = await driver.wait(until.elementLocated(
+      By.id("get-results")
+    ), 1000);
+    await driver.wait(until.elementTextContains(results, 'guid'), 5000);
+    const output = await results.getText();
+    expect(JSON.parse(output)).to.deep.equal(mockLogin);
+  });
+
+  it("browser.experiments.logins.getAll should return the login in an array", async () => {
+    await addLogin();
+
+    await loadTestPage();
+    await clickButton("get-all");
+
+    const results = await driver.wait(until.elementLocated(
+      By.id("get-all-results")
+    ), 1000);
+    await driver.wait(until.elementTextContains(results, 'guid'), 5000);
+    const output = await results.getText();
+    expect(JSON.parse(output)).to.deep.equal([mockLogin]);
+  });
+
+  it("browser.experiments.logins.update should update the username", async () => {
+    await addLogin();
+
+    await loadTestPage();
+    await clickButton("update");
+
+    const logins = await getLogins();
+    expect(logins[0].username).to.equal(mockLogin.username);
+  });
+
+  it("browser.experiments.logins.touch should update the timesUsed and lastTimeUsed values", async () => {
+    await addLogin();
+
+    await loadTestPage();
+    await clickButton("touch");
+
+    const logins = await getLogins();
+    expect(logins[0].timesUsed).to.equal(2);
   });
 });
