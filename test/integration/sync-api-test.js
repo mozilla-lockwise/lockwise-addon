c95aa6597f4817b439a4904614bdcfde2414a791
Jared Hirsch <ohai@6a68.net>
2019-01-14 10:45:41 -0800

    Add Sync API (#23)

---
+++ test/integration/sync-api-test.js
@@ -0,0 +1,310 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import getWebExtension from "./driver";
+import chai, { expect } from "chai";
+import chaiAsPromised from "chai-as-promised";
+
+chai.use(chaiAsPromised);
+
+describe("sync API", () => {
+  let webext, driver, webdriver, By, until;
+
+  const clickButton = async (id) => {
+    await webext.inContent();
+    const button = await driver.wait(until.elementLocated(By.id(id)), 5000);
+    await button.click();
+  };
+
+  const getResults = async (id) => {
+    await webext.inContent();
+    const results = await driver.wait(until.elementLocated(By.id(id)), 5000);
+    await driver.wait(until.elementTextMatches(results, /[a-z]/));
+    const output = results.getText();
+    return output;
+  };
+
+  const loadTestPage = async () => {
+    await webext.inContent();
+    await driver.get(webext.url("/test/integration/test-pages/sync-api.html"));
+  };
+
+
+  const getProfileInfo = async () => {
+    await clickButton("get-profile-info");
+    const profileInfo = await getResults("get-profile-info-results");
+    return profileInfo;
+  };
+
+  const registerOnUserProfileChangedListener = async () => {
+    await clickButton("register-listener");
+  };
+
+  const getUserProfileChangedEvents = async () => {
+    const results = await getResults("register-listener-results");
+    return results;
+  };
+
+  const getPasswordsPref = async () => {
+    await clickButton("check-passwords-pref");
+    const output = await getResults("check-passwords-pref-results");
+    return output;
+  };
+
+  // Force an update of the UIState. This method exists on the UIState API to
+  // aid with rapid state transitions during testing.
+  const refreshUIState = async () => {
+    await webext.inChrome();
+    await driver.executeAsyncScript(`(async () => {
+      ChromeUtils.defineModuleGetter(this, "UIState",
+                                     "resource://services-sync/UIState.jsm");
+      await UIState.refresh();
+    })().then(arguments[0]);
+    `);
+  };
+
+  // Fire the Firefox-internal event that causes the onUserProfileChanged API
+  // to fire (if the UIState has changed).
+  const triggerInternalSyncEvent = async () => {
+    await webext.inChrome();
+    await driver.executeScript(`
+      ChromeUtils.import("resource://gre/modules/Services.jsm");
+      Services.obs.notifyObservers(null, "sync-ui-state:update");
+    `);
+  };
+
+  /**
+   * The existing Firefox test code mocks out the UIState by setting the
+   * UIState._internal.fxAccounts values, then calling UIState.refresh()
+   * to trigger updates based on the mock data. The following helper methods
+   * are used to take the same approach in our tests.
+   *
+   * Reference: https://searchfox.org/mozilla-central/source/services/sync/tests/unit/test_uistate.js
+   */
+
+  // Mock out the logged in case, with default values.
+  const setLoggedIn = async () => {
+    await webext.inChrome();
+    await driver.executeAsyncScript(`(async () => {
+      const testTime = new Date("January 1, 2019");
+      ChromeUtils.import("resource://gre/modules/Services.jsm");
+      Services.prefs.setCharPref("services.sync.lastSync", testTime);
+      UIState._internal.syncing = false;
+
+      UIState._internal.fxAccounts = {
+        getSignedInUser: () => Promise.resolve({
+          uid: "0123456789abcdef0123456789abcdef",
+          verified: true,
+          email: "foo@example.com",
+        }),
+        // Use the defaults for displayName and avatarURL, to ensure we
+        // handle them correctly.
+        getSignedInUserProfile: () => Promise.resolve({
+          displayName: undefined,
+          uid: "0123456789abcdef0123456789abcdef",
+          avatar: "https://firefoxusercontent.com/00000000000000000000000000000000",
+        }),
+        hasLocalSession: () => Promise.resolve(true),
+      };
+
+      await UIState.refresh();
+    })().then(arguments[0]);
+    `);
+  };
+
+  // Mock out the state where the user is logged out or has never logged in.
+  const setNotConfigured = async () => {
+    await webext.inChrome();
+    await driver.executeAsyncScript(`(async () => {
+      const testTime = new Date("January 1, 2019");
+      ChromeUtils.import("resource://gre/modules/Services.jsm");
+      Services.prefs.setCharPref("services.sync.lastSync", testTime);
+      UIState._internal.syncing = false;
+
+      UIState._internal.fxAccounts = {
+        getSignedInUser: () => Promise.resolve(null),
+        getSignedInUserProfile: () => Promise.resolve(null),
+      };
+
+      await UIState.refresh();
+    })().then(arguments[0]);
+    `);
+  };
+
+  // Mock out the state where the user is logged in, but the password was
+  // changed on another device.
+  const setLoginFailed = async () => {
+    await webext.inChrome();
+    await driver.executeAsyncScript(`(async () => {
+      const testTime = new Date("January 1, 2019");
+      ChromeUtils.import("resource://gre/modules/Services.jsm");
+      Services.prefs.setCharPref("services.sync.lastSync", testTime);
+      UIState._internal.syncing = false;
+
+      Services.prefs.setStringPref("services.sync.username", "foo@example.com");
+      UIState._internal.fxAccounts = {
+        getSignedInUser: () => Promise.resolve({
+          email: "foo@example.com",
+          uid: "0123456789abcdef0123456789abcdef",
+          verified: true,
+        }),
+        getSignedInUserProfile: () => Promise.resolve(null),
+        hasLocalSession: () => Promise.resolve(),
+      };
+
+      await UIState.refresh();
+    })().then(arguments[0]);
+    `);
+  };
+
+  // Mock out the state where the user is logged in, but the email is
+  // unverified.
+  const setNotVerified = async () => {
+    await webext.inChrome();
+    await driver.executeAsyncScript(`(async () => {
+      const testTime = new Date("January 1, 2019");
+      ChromeUtils.import("resource://gre/modules/Services.jsm");
+      Services.prefs.setCharPref("services.sync.lastSync", testTime);
+      UIState._internal.syncing = false;
+
+      UIState._internal.fxAccounts = {
+        getSignedInUser: () => Promise.resolve({
+          verified: false,
+          uid: "0123456789abcdef0123456789abcdef",
+          email: "foo@example.com",
+        }),
+        getSignedInUserProfile: () => Promise.resolve(null),
+        hasLocalSession: () => Promise.resolve(true),
+      };
+
+      await UIState.refresh();
+    })().then(arguments[0]);
+    `);
+  };
+
+  const MockUserProfileInfo = {
+    loggedIn: {
+      status: "ok",
+      avatar: "https://firefoxusercontent.com/00000000000000000000000000000000",
+      email: "foo@example.com",
+      id: "0123456789abcdef0123456789abcdef",
+      displayName: null,
+    },
+    error: {
+      status: "error",
+      avatar: null,
+      email: "foo@example.com",
+      id: "0123456789abcdef0123456789abcdef",
+      displayName: null,
+    },
+  };
+
+  before(async () => {
+    webext = await getWebExtension();
+    await webext.start();
+    driver = await webext.driver;
+    webdriver = webext.webdriver;
+    By = webdriver.By;
+    until = webdriver.until;
+
+    await webext.inChrome();
+    await driver.executeScript(`
+      ChromeUtils.import("resource://gre/modules/Services.jsm");
+      ChromeUtils.import("resource://services-sync/UIState.jsm");
+    `);
+  });
+
+  after(async () => {
+    await webext.stop();
+  });
+
+  afterEach(async () => {
+    await webext.inChrome();
+    await driver.executeScript(`
+      ChromeUtils.import("resource://gre/modules/Services.jsm");
+      ChromeUtils.import("resource://services-sync/UIState.jsm");
+      Services.prefs.clearUserPref("services.sync.lastSync");
+      Services.prefs.clearUserPref("services.sync.username");
+      UIState._internal.fxAccounts = window.fxAccountsOrig;
+      UIState.reset();
+    `);
+  });
+
+  describe("browser.experiments.sync.getCurrentState", () => {
+    it("should correctly check the state of logins sync ", async () => {
+      await webext.inChrome();
+      driver.executeScript(`
+        ChromeUtils.import("resource://gre/modules/Services.jsm");
+        Services.prefs.setBoolPref("services.sync.engine.passwords", false);
+      `);
+      await refreshUIState();
+      await loadTestPage();
+      let result = await getPasswordsPref();
+      expect(JSON.parse(result)).to.be.false;
+
+      await webext.inChrome();
+      driver.executeScript(`
+        ChromeUtils.import("resource://gre/modules/Services.jsm");
+        Services.prefs.setBoolPref("services.sync.engine.passwords", true);
+      `);
+      await refreshUIState();
+      result = await getPasswordsPref();
+      expect(JSON.parse(result)).to.be.true;
+    });
+  });
+
+  describe("browser.experiments.sync.getUserProfileInfo", () => {
+    it("should return null if the user is not logged in", async () => {
+      await setNotConfigured();
+      await loadTestPage();
+      const profileInfo = await getProfileInfo();
+      expect(JSON.parse(profileInfo)).to.be.null;
+    });
+    it("should return profile info, if the user is logged in", async () => {
+      await setLoggedIn();
+      await loadTestPage();
+      const profileInfo = await getProfileInfo();
+      expect(JSON.parse(profileInfo)).to.deep.equal(MockUserProfileInfo.loggedIn);
+    });
+    it("should return a profile with correct email and error status, if the user's login is stale", async () => {
+      await setLoginFailed();
+      await loadTestPage();
+      const profileInfo = await getProfileInfo();
+      expect(JSON.parse(profileInfo)).to.deep.equal(MockUserProfileInfo.error);
+    });
+    it("should return a profile with correct email and error status, if the user's email is unverified", async () => {
+      await setNotVerified();
+      await loadTestPage();
+      const profileInfo = await getProfileInfo();
+      expect(JSON.parse(profileInfo)).to.deep.equal(MockUserProfileInfo.error);
+    });
+  });
+
+  describe("browser.experiments.sync.onUserProfileChanged", () => {
+    it("should notify observers when the internal sync event fires", async () => {
+      // This test simulates two aspects of Firefox Sync code behavior.
+      // In production, when the FxA/Sync state changes, the Firefox Sync code
+      // both updates the UIState, and fires a "sync-ui-state:update" internal
+      // (nsIObserver) event. Note that the Firefox Sync code fires multiple
+      // internal events for a single FxA/Sync state change. The Lockbox sync
+      // API listens for this internal "sync-ui-state:update" event, filters
+      // out duplicate events by comparing the new UIState against the previous
+      // UIState, then notifies onUserProfileChanged observers if the state
+      // has really changed. To test this effectively, we need to both simulate
+      // a legit UIState change, and also fire the "sync-ui-state:update"
+      // event to trigger the update process.
+      await loadTestPage();
+      await registerOnUserProfileChangedListener();
+
+      await setLoggedIn();
+      await refreshUIState();
+      await triggerInternalSyncEvent();
+
+      const events = await getUserProfileChangedEvents();
+      expect(JSON.parse(events)).to.deep.equal([MockUserProfileInfo.loggedIn]);
+    });
+  });
+});
---
+++ test/integration/test-pages/sync-api.js
@@ -0,0 +1,28 @@
+// Returns a function that logs to a DOM element.
+const getLogger = (id) => {
+  return (result) => {
+    let target = document.querySelector(`#${id}-results`);
+    target.textContent = result instanceof Error ? result.toString() : JSON.stringify(result);
+  };
+};
+
+document.querySelector("#get-profile-info").addEventListener("click", () => {
+  const log = getLogger("get-profile-info");
+  browser.experiments.sync.getUserProfileInfo().
+    then(log, log);
+});
+
+document.querySelector("#check-passwords-pref").addEventListener("click", () => {
+  const log = getLogger("check-passwords-pref");
+  browser.experiments.sync.getCurrentState("passwords").
+    then(log, log);
+});
+
+document.querySelector("#register-listener").addEventListener("click", () => {
+  const events = [];
+  const log = getLogger("register-listener");
+  browser.experiments.sync.onUserProfileChanged.addListener(update => {
+    events.push(update);
+    log(events);
+  });
+});
