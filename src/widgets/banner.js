7bce2f002bc830bd6b0d7cc00cb1656e56cf2d9e
Matthew A. Miller <linuxwolf@outer-planes.net>
2019-03-20 11:46:43 -0600

    unit tests for banner

--- src/widgets/banner.js
+++ src/widgets/banner.js
@@ -8,13 +8,17 @@ import PropTypes from "prop-types";
 import React from "react";
 
 import styles from "./banner.css";
+import { classNames } from "../common";
 
 // TODO: add "close banner" hooks (https://github.com/mozilla-lockbox/lockbox-addon/issues/95)
 
-export default function Banner({ children }) {
-  return <div className={styles.banner}>{children}</div>;
+export default function Banner({ className, children }) {
+  const fullClassName = classNames([ styles.banner, className ]);
+
+  return <div className={fullClassName}>{children}</div>;
 }
 
 Banner.propTypes = {
+  className: PropTypes.string,
   children: PropTypes.node,
 };
---
+++ test/unit/widgets/banner-test.js
@@ -0,0 +1,38 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import chai, { expect } from "chai";
+import chaiEnzyme from "chai-enzyme";
+import React from "react";
+import sinonChai from "sinon-chai";
+
+import { mount } from "test/unit/enzyme";
+import Banner from "src/widgets/banner";
+
+chai.use(chaiEnzyme());
+chai.use(sinonChai);
+
+describe.only("widgets > <Banner/>", () => {
+  it("render", () => {
+    const wrapper = mount(
+      <Banner>
+        <span>hello</span>
+      </Banner>
+    );
+    expect(wrapper.find("span")).to.have.text("hello");
+  });
+
+  it("merge class names", () => {
+    const wrapper = mount(
+      <Banner className="bar">
+        <span>foo</span>
+      </Banner>
+    );
+    expect(wrapper.find("div")).prop("className").to.match(
+      /^\S*banner\S* bar$/
+    );
+  });
+});
