ab1e723fc1c68f959b337b6bbab1a72d95ca2dda
dave justice <djustice@mozilla.com>
2019-04-23 18:14:56 -0400

    Set an item on management page load to avoid blank home page
    - fixes #148

--- src/list/manage/index.js
+++ src/list/manage/index.js
@@ -10,12 +10,14 @@ import thunk from "redux-thunk";
 
 import AppLocalizationProvider from "../../l10n";
 import App from "./components/app";
-import { listItems, getProfile, filterItems } from "../actions";
+import { listItems, getProfile, filterItems, requestSelectItem } from "../actions";
 import reducer from "./reducers";
 import initializeMessagePorts from "../message-ports";
 import telemetryLogger from "./telemetry";
 import { saveSort, loadSort } from "./sort-middleware";
 import openLink from "../open-link-middleware";
+
+import { getSort } from "../sort";
 
 const applyQueryFilter = () => {
   // parse query params ...
@@ -34,10 +36,17 @@ let store;
   store = createStore(reducer, preloadedState, applyMiddleware(
     thunk, telemetryLogger, saveSort, openLink,
   ));
-  store.dispatch(listItems());
+  await store.dispatch(listItems());
   initializeMessagePorts(store);
   store.dispatch(getProfile());
   applyQueryFilter();
+
+  // select a default item so the home view is not empty
+  const cache = store.getState().cache;
+  if (cache.items.length) {
+    const sortedList = cache.items.sort(getSort(cache.sort));
+    store.dispatch(requestSelectItem(sortedList[0].id));
+  }
 
   ReactDOM.render(
     <Provider store={store}>
